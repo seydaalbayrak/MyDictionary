a6b3fbe565e46bf37cf15ce6ad4e75f8ae53fc18
Şeyda Albayrak <seydaalbayrak02@outlook.com>
2021-03-11 21:50:34 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+          
+
+            MyDictionary<string, int> myDictionary = new MyDictionary<string, int>();
+            myDictionary.Add("txt", 25);
+            myDictionary.Add("txt2", 30);
+            myDictionary.Add("txt3", 35);
+
+            Console.WriteLine(myDictionary.Count());
+
+           
+        }
+    }
+}
